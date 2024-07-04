948bce9cb9ecf47becd986725a8629a44ad6ed20
Faisal Kabir Galib <faisal@dana.money>
2024-07-05 00:23:39 +0600

    Initial Commit

---
+++ KYCVideo/KYCVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KYCVideoApp.swift
+//  KYCVideo
+//
+//  Created by Faisal Kabir Galib on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KYCVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
