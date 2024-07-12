cfe43023b32689930b7dd6bdb0f8b7c814a718a6
Md Mahfuzur Rahman <mrj.cse15@gmail.com>
2024-07-12 23:20:14 +0600

    added server url

--- KYCVideo/CameraView.swift
+++ KYCVideo/CameraView.swift
@@ -32,36 +32,7 @@ struct CameraView: UIViewControllerRepresentable {
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
-    func uploadVideoToServer(fileURL: URL) {
-        let url = URL(string: "baseurl/upload")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
         
-        let boundary = "Boundary-\(UUID().uuidString)"
-        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        
-        var data = Data()
-        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-        data.append("Content-Disposition: form-data; name=\"video\"; filename=\"\(fileURL.lastPathComponent)\"\r\n".data(using: .utf8)!)
-        data.append("Content-Type: video/mp4\r\n\r\n".data(using: .utf8)!)
-        data.append(try! Data(contentsOf: fileURL))
-        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
-        
-        request.httpBody = data
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                print("Error: \(error)")
-                return
-            }
-            // Handle the response here
-        }.resume()
-    }
-
-    
-    
-    
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
@@ -87,12 +58,51 @@ struct CameraView: UIViewControllerRepresentable {
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let videoURL = info[.mediaURL] as? URL {
                 print(videoURL)
+                uploadVideoToServer(fileURL: videoURL)
             }
             picker.dismiss(animated: true)
         }
         
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             picker.dismiss(animated: true)
+        }
+        
+        func uploadVideoToServer(fileURL: URL) {
+            let url = URL(string: "https://ekycapi.bitanon.xyz/api/face/liveness")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            
+            let boundary = "Boundary-\(UUID().uuidString)"
+            request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+            request.setValue("Bearer eyJhbGciOiJSUzI1NiIsInppcCI6IkRFRiJ9.eJxVzDELwjAQhuH_cnMHLWpsN0WHgiYgdcuS2isEr40kDQil_90z2MHxnuN7J8D3C8q1yPO9KHabIoMQGyhh0mBbDWW-EmKbaYgB_WB6ZNLgnRs1sHaRSC56WxR7Y4lp4C-f5B5P_LY6QwEZWhtMQ__keDOkzLGqD1LJVPKOfm2l6iSjRZ_kLi_VtarPJw0zzB879D5m.uFu05q_zVCVcNUK7VJ8IMFSluy0k09QjDvfhq_VXDHqzeIyQ6oU2b4OR9_etslXy63fUUvWNMDqDOoROVmT2Wd7fclzSM750qMJUm8ETigYzJkQowpeikjy9FlBgzGGMRCBiijpqgBMclLt1qkbyG4nCk_j2u25zTweDxnSCWz2Ns1pyjt8aeHnqUYo9iLcZY0XLlYM-xBkbGPbfKhygVRh65OPvNVLV2cN6Biyp5bwWPnpKzhagyndsAoiIRRRJ7Pe9xcbYVcXoxB0iqLhKzfULZBbRUAAlIvf5WCPb0kXV1lR8Qhv4qv-rAd7eghoa70bzSwKd6YbFJ4yP8ou0bA", forHTTPHeaderField: "Authorization")
+            
+            var data = Data()
+            data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+            data.append("Content-Disposition: form-data; name=\"face\"; filename=\"\(fileURL.lastPathComponent)\"\r\n".data(using: .utf8)!)
+            data.append("Content-Type: video/mp4\r\n\r\n".data(using: .utf8)!)
+            data.append(try! Data(contentsOf: fileURL))
+            data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+            
+            request.httpBody = data
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                if let httpResponse = response as? HTTPURLResponse {
+                        print("Response Received: \(httpResponse)")
+                        // To print status code
+                        print("Status Code: \(httpResponse.statusCode)")
+                        // To print all headers
+                        print("Headers: \(httpResponse.allHeaderFields)")
+                    }
+                    
+                    // Optionally, if you want to print the body of the response
+                    if let data = data, let body = String(data: data, encoding: .utf8) {
+                        print("Body: \(body)")
+                    }
+            }.resume()
         }
     }
 }
